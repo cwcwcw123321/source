da92dd6f2ad75ada1869a1ba9ede9679edca90c2
ThinkPad <qihaoh@163.com>
2019-11-08 23:09:46 +0800

    signalslot

--- new_PyQt5/10/run_ui_10.py
+++ new_PyQt5/10/run_ui_10.py
@@ -1,8 +1,10 @@
 import ui_10
 import sys
 from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtCore import Qt
 
 if __name__ == '__main__':
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling) #设置高清屏幕属性  ？？   设置出来界面显示正常
     app=QApplication(sys.argv)
     w=QMainWindow()
     ui=ui_10.Ui_MainWindow()
